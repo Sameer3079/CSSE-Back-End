1d3ecfa4db86ab0d6de50e3950cb98993d1b76a5
Sameer3079 <smrbasil4@gmail.com>
2018-09-26 20:17:37 +0530

    Removed unnecessary imports

--- src/main/java/csse/construction_management/controller/EmployeeController.java
+++ src/main/java/csse/construction_management/controller/EmployeeController.java
@@ -4,9 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +12,7 @@ import csse.construction_management.models.EmployeeModel;
 import csse.construction_management.repository.EmployeeRepository;
 
 @RestController("/employees")
-public class Employee {
+public class EmployeeController {
 
 	@Autowired
 	EmployeeRepository employeeRepository;
