ca39c4b97eb733bd80fdde4a7f01a82f7218f760
sachithkk <tharakakk@gmail.com>
2018-09-30 07:54:58 +0530

    create site model

---
+++ Model/Site.js
@@ -0,0 +1,31 @@
+const Mongoose          = require("mongoose");
+const Float             = require("mongoose-float");
+const Schema            = Mongoose.Schema;
+
+const SiteShema = new Schema({
+
+    siteName : {
+        type : String,
+        require : true
+    },
+    address : {
+        type : String,
+        require : true
+    },
+    items : {
+        type : Array,
+        require : true
+    },
+    storageCapacity :{
+        type :Float,
+        require : true
+    },
+    currentCapacity : {
+        type : Float,
+        require :true
+    }
+});
+
+Mongoose.model('Site' , SiteShema);
+
+module.exports = Mongoose;
