edf61501f9fbcec5bdd0f18483a75c1032f9d4a1
sachithkk <tharakakk@gmail.com>
2018-09-30 09:38:40 +0530

    Add order route

--- Routes.js
+++ Routes.js
@@ -3,10 +3,13 @@ const Routes            = Express.Router();
 const PaymentRoute      = require('./Payment/Payment.Route');
 const UserRoute         = require('./Users/User.Route');
 const ItemRoute         = require("./Item/Item.Route");
-
+const SiteRoute         = require('./Site/Site.Route');
+const OrderRoute        = require('./Order/Order.Route');
 
 Routes.use('/payments' , PaymentRoute);
 Routes.use('/users' , UserRoute);
-Routes.use('/item' , ItemRoute);
+Routes.use('/items' , ItemRoute);
+Routes.use('/sites' , SiteRoute);
+Routes.use('/orders' , OrderRoute);
 
 module.exports = Routes;
