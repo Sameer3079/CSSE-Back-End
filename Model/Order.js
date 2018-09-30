301837d9ff877623210d3834cf9096b3b3b55780
sachithkk <tharakakk@gmail.com>
2018-09-30 09:36:46 +0530

    Create order model

---
+++ Model/Order.js
@@ -0,0 +1,35 @@
+const Mongoose          = require("mongoose");
+const Flaot             = require("mongoose-float");
+const Schema            = Mongoose.Schema;
+
+const OrderShema = new Schema({
+
+    sequential : {
+        type : String,
+        required : true
+    },
+    items : {
+        type : Array,
+        required : true
+    },
+    orderStatus : {
+        type : String,
+        required : true
+    },
+    orderDate : {
+        type: Date,
+        require : true
+    },
+    isDraftPurchaseOrder : {
+        type : Boolean,
+        required : true
+    },
+    onHold : {
+        type : Boolean,
+        required : true
+    }
+});
+
+Mongoose.model('Order' , OrderShema);
+
+module.exports = Mongoose;
